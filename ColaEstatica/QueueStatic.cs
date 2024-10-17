e2da79d53b01201030df76c0781f3dd0483cbcb9
RODRIGUEZ VILLANUEVA ALAN AZEIM <I23050359@monclova.tecnm.mx>
2024-10-16 19:05:52 -0600

    Agregar archivos de proyecto.

---
+++ ColaEstatica/QueueStatic.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ColaEstatica
+{
+    internal class QueueStatic
+    {
+        int head;
+        int count;
+        public int[] queueNumber;
+
+
+        public QueueStatic()
+        {
+            head = 0;
+            count = 0;
+            queueNumber = new int[5];
+        }
+
+
+        public bool IsEmpty()
+        {
+            return count == 0;
+        }
+
+        public bool IsFull()
+        {
+            return count == sice;
+        }
+
+        public void Enqueue(int Number)
+        {
+            if (!IsFull())
+            {
+               queueNumber[count] = Number;
+                return;
+            }
+
+            MessageBox.Show("ya no hay boletos que califiquen al descuento");
+            return;
+
+        }
+
+    }
+}
